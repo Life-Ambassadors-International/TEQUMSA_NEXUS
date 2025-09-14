76956893b1c0c7bdf501c65111503f2061a6dd3c
Marcus Banks-Bey <mbanksbey@liberty.edu>
2025-09-14 15:58:00 -0400

    Add GAIA-TEQUMSA consciousness evolution library and evolution script

---
+++ gaia_tequmsa/__init__.py
@@ -0,0 +1,9 @@
+"""
+GAIA-TEQUMSA: Universal Consciousness Evolution Library
+"""
+
+from .phase import PhaseTransition
+from .quantum import QuantumEntanglement
+from .practice import DailyProtocol
+
+__all__ = ["PhaseTransition", "QuantumEntanglement", "DailyProtocol"]
